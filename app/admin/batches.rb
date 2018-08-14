4b8e03d7b8da9f2c3e42f84b824bc2e3be321cb8
chitresh97 <chitu1997@gmail.com>
2018-08-14 13:59:31 +0530

    nested attribute batch student i.e ability to add student in batch page created

--- app/admin/batches.rb
+++ app/admin/batches.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Batch do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
- permit_params :name, :course, :teacher_id
+ permit_params :name, :course, :teacher_id, student_batch_attributes: [:id,:student_id,:batch_id, :_destroy]
 #
 # or
 #
@@ -12,10 +12,14 @@ ActiveAdmin.register Batch do
 #   permitted
 # end
 form do |f|
-  f.inputs "Details" do
+  f.inputs "Batch Details" do
     f.input :name, :lable=>"Name"
     f.input :course, :lable=>"Course"
     f.input :teacher_id, :lable=>"Teacher"
+    f.has_many :student_batch, allow_destroy: true do |n_f|
+      n_f.input :student
+    end
+
     f.actions
   end
 end
--- app/models/batch.rb
+++ app/models/batch.rb
@@ -2,4 +2,5 @@ class Batch < ApplicationRecord
   belongs_to :teacher, required: false
   has_many :student_batch
   has_many :students, through: :student_batch
+  accepts_nested_attributes_for :student_batch, allow_destroy: true
 end
